19291461c92abd3bda5bfa2b768ef1ae47c07079
masmian <noelmzs@gmail.com>
2024-05-23 09:30:54 -0600

    proyecto con vistas creadas

---
+++ config/app.php
@@ -0,0 +1,8 @@
+<?php 
+    const serve_url = "http://localhost/prestamos/";
+
+    const company = "SISTEMA PRESTAMOS";
+
+    const moneda = "$";
+
+    date_default_timezone_set("America/Managua");
---
+++ config/server.php
@@ -0,0 +1,11 @@
+<?php
+    const SERVER = "localhost";
+    const DB = "prestamos";
+    const USER = "root";
+    const PASS ="Claro2023*";
+    
+    const SGDB = "mysql:host=".SERVER.";dbname=".dB;
+
+    const METHOD = "AES-256-CBC";
+    const SECRET_KEY ='$PRESTAMOS@2024*';
+    const SECRET_IV = '202124';
